a8fd239e71de765640a20708e2b399a268689773
Eason Lee <demolure@outlook.com>
2025-11-29 17:07:18 +0800

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/server/CMakeLists.txt
    	modified:   src/server/chatservice.cpp

--- src/server/chatservice.cpp
+++ src/server/chatservice.cpp
@@ -223,17 +223,45 @@ void ChatService::addFriend(const TcpConnectionPtr &conn, json &js, Timestamp ti
 // 创建群组业务
 void ChatService::createGroup(const TcpConnectionPtr &conn, json &js, Timestamp time)
 {
-    // TODO
-    // _groupModel.createGroup();
+    int userid = js["id"].get<int>();
+    string name = js["groupname"];
+    string desc = js["groupdesc"];
+
+    Group group(-1, name, desc);
+    if (_groupModel.createGroup(group))
+    {
+        _groupModel.addGroup(userid, group.getId(), "creator");
+    }
 }
+
 // 加入群组业务
 void ChatService::addGroup(const TcpConnectionPtr &conn, json &js, Timestamp time)
 {
-    // TODO
-    // _groupModel.addGroup();
+    int userid = js["id"].get<int>();
+    int groupid = js["groupid"].get<int>();
+
+    _groupModel.addGroup(userid, groupid, "normal");
 }
+
 // 群组聊天业务
 void ChatService::groupChat(const TcpConnectionPtr &conn, json &js, Timestamp time)
 {
-    // TODO
+    int userid = js["id"].get<int>();
+    int groupid = js["groupid"].get<int>();
+    vector<int> vec = _groupModel.queryGroupUsers(userid, groupid);
+
+    lock_guard<mutex> lock(_connMutex);
+    for (int id : vec)
+    {
+        auto it = _userConnMap.find(id);
+        if (it != _userConnMap.end())
+        {
+            it->second->send(js.dump());
+        }
+        else
+        {
+            // 存储离线消息
+            _offlineMsgModel.insert(id, js.dump());
+        }
+    }
 }
